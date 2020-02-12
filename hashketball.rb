6657faf5f8c921fa2526cf312d3420ade692e577
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-12 23:57:55 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,133 @@
-# Write your code here!
+def game_hash
+  hashketball = {
+    home: { team_name: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: [
+              { player_name: "Alan Anderson",
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slam_dunks: 1},
+
+              { player_name: "Reggie Evans",
+                number: 30,
+                shoe: 14,
+                points: 12,
+                rebounds: 12,
+                assists: 12,
+                steals: 12,
+                blocks: 12,
+                slam_dunks: 7},
+
+              { player_name: "Brook Lopez",
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: },
+
+              { player_name: "Mason Plumlee",
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: },
+
+              { player_name: "Jason Terry",
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: }
+            ]
+
+    },
+
+    away: { team_name: "Charlotte Hornets",
+            colors: ["Turquoise", "Purple"],
+            players: [
+              { player_name: 'Jeff Adrien',
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slam_dunks: 2 },
+
+              { player_name: ,
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: },
+
+              { player_name: ,
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: },
+
+              { player_name: ,
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: },
+                
+              { player_name: ,
+                number: ,
+                shoe: ,
+                points: ,
+                rebounds: ,
+                assists: ,
+                steals: ,
+                blocks: ,
+                slam_dunks: }
+            ]
+    }
+  }
+end
+
+{
+    away: { team_name: 'Charlotte Hornets',
+            colors: %w[Turquoise Purple],
+            players: [
+              { player_name: 'Jeff Adrien',
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slam_dunks: 2 },
+              { player_name: 'Bismack Biyombo',
+                number: 0,
 
 
 
@@ -8,4 +137,4 @@
 
 
 
-
+# rspec spec/hashketball_spec.rb -e game_hash
