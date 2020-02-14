42831bfbf4b0cf2a7cfa616d1c49b356e8689434
Abubaker Noorzi <anoorzi123@gmail.com>
2020-02-14 16:01:43 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,115 @@
 # Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  {
+    :away => { :team_name => "Charlette Hornets",
+                :colors => ["Turquoise","Purple"],
+                :players => [
+                  { :player_name => "Jeff Adrien",
+                    :number => 4,
+                    :shoe => 18,
+                    :points => 10,
+                    :rebounds => 1,
+                    :assists => 1,
+                    :steals => 2,
+                    :blocks => 7,
+                    :slam_dunks => 2
+                  },
+                  { :player_name => "Bismack Biyambo",
+                    :number => 0,
+                    :shoe => 16,
+                    :points => 12,
+                    :rebounds => 4,
+                    :assists => 7,
+                    :steals => 22,
+                    :blocks => 15,
+                    :slam_dunks => 10
+                  },
+                  { :player_name => "Baker noorzi",
+                    :number => 11,
+                    :shoe => 8,
+                    :points => 11,
+                    :rebounds => 4,
+                    :assists => 6,
+                    :steals => 5,
+                    :blocks => 7,
+                    :slam_dunks => 3
+                  },
+                  { :player_name => "Mr Smith",
+                    :number => 88,
+                    :shoe => 13,
+                    :points => 14,
+                    :rebounds => 15,
+                    :assists => 3,
+                    :steals => 2,
+                    :blocks => 5,
+                    :slam_dunks => 5
+                  },
+                  { :player_name => "basgh Smith",
+                    :number => 7,
+                    :shoe => 13,
+                    :points => 5,
+                    :rebounds => 5,
+                    :assists => 1,
+                    :steals => 1,
+                    :blocks => 5,
+                    :slam_dunks => 1
+                  }
+                ]
+              },
+              :away => { :team_name => "Knicks",
+                          :colors => ["Blue","Purple"],
+                          :players => [
+                            { :player_name => "james deen",
+                              :number => 40,
+                              :shoe => 18,
+                              :points => 10,
+                              :rebounds => 1,
+                              :assists => 1,
+                              :steals => 2,
+                              :blocks => 7,
+                              :slam_dunks => 2
+                            },
+                            { :player_name => "baby big",
+                              :number => 0,
+                              :shoe => 16,
+                              :points => 12,
+                              :rebounds => 4,
+                              :assists => 7,
+                              :steals => 22,
+                              :blocks => 15,
+                              :slam_dunks => 10
+                            },
+                            { :player_name => "tupak shakur",
+                              :number => 11,
+                              :shoe => 8,
+                              :points => 11,
+                              :rebounds => 4,
+                              :assists => 6,
+                              :steals => 5,
+                              :blocks => 7,
+                              :slam_dunks => 3
+                            },
+                            { :player_name => "miss Smith",
+                              :number => 88,
+                              :shoe => 13,
+                              :points => 14,
+                              :rebounds => 15,
+                              :assists => 3,
+                              :steals => 2,
+                              :blocks => 5,
+                              :slam_dunks => 5
+                            },
+                            { :player_name => "bash bash",
+                              :number => 7,
+                              :shoe => 13,
+                              :points => 5,
+                              :rebounds => 5,
+                              :assists => 1,
+                              :steals => 1,
+                              :blocks => 5,
+                              :slam_dunks => 1
+                            }
+                          ]
+              }
+            }
+          end
